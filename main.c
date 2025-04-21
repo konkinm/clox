4eb85f92159c3d92623be5668111dc921c47e8a6
MaxKonkin <knkn.max@yandex.ru>
2025-04-21 11:46:16 +0300

    INIT

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
+
+int main(int argc, const char* argv[]) {
+    Chunk chunk;
+    initChunk(&chunk);
+    whriteChunk(&chunk, OP_RETURN);
+	
+	dissassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
+    return 0;
+}
